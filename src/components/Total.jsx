6ed68cbea1544339d9afa1db6b419469fa2b7505
Aapo <aapo.jutila@aalto.fi>
2024-01-12 16:10:49 +0200

    2.1-2.2 kurssitiedot step7

---
+++ src/components/Total.jsx
@@ -0,0 +1,8 @@
+const Total = ({parts}) => {
+  const total = parts.reduce((acc, part) => acc + part.exercises, 0)
+  return (
+    <p><b>total of {total} exercises</b></p>
+  )
+}
+
+export default Total
