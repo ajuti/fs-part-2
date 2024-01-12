5f577cbeffe51bbc1f723570f31df83ef1ffcdb7
Aapo <aapo.jutila@aalto.fi>
2024-01-12 16:21:08 +0200

    2.3-2.5 kurssitiedot

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,13 +1,13 @@
-import Header from "./Header.jsx"
+import Subheader from "./Subheader.jsx"
 import Content from "./Content.jsx"
 import Total from "./Total.jsx"
 
 const Course = ({course}) => {
   return (
     <> 
-      <Header title={course.name} />
+      <Subheader title={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Total parts={course.parts}/>
     </>
   )
 }
---
+++ src/components/Subheader.jsx
@@ -0,0 +1,9 @@
+const Subheader = ({title}) => {
+  return (
+    <h2>
+      {title}
+    </h2>
+  )
+}
+
+export default Subheader
