c1a76866c01ba2ec2877cb0469a24b3d30159dc7
Aapo Jutila <aapo.jutila01@gmail.com>
2024-01-15 23:41:14 +0200

    restructures + puhelinluettelo step1

---
+++ puhelinluettelo/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObj = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(nameObj))
+    setNewName("")
+  }
+
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(obj => 
+        <p key={obj.name}>{obj.name}</p>   
+      )}
+    </div>
+  )
+
+}
+
+export default App
